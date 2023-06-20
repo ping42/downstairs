aac70617a94615b5a2b002f70df99c56f58f4112
Vincent Wong <>
2023-06-20 08:51:13 +0100

    remove # from url

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,8 @@
 import 'package:downstairs/app/app.dart';
 import 'package:downstairs/bootstrap.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   bootstrap(() => const App());
 }
