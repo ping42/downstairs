b1a9bff09448c345cc7efbadc8411def3e0799d8
Vincent Wong <>
2023-08-02 11:58:25 +0100

    put localization files to git to allow CICD build

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,8 +1,8 @@
+import 'package:downstairs/l10n/gen_l10n/app_localizations.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+export 'package:downstairs/l10n/gen_l10n/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this);
+  AppLocalizations get l10n => AppLocalizations.of(this)!;
 }
